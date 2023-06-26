2b1c4c0a6711ffa8f1a35352637b8b321444801e
Anton Kruglyakov <akrugs@mail.ru>
2023-06-27 00:25:12 +0500

    ДЗ1, 2 задание

--- DZ1/Dz1/src/main/java/org/example/Dz1.java
+++ DZ1/Dz1/src/main/java/org/example/Dz1.java
@@ -1,12 +1,19 @@
 package org.example;
 import java.util.Random;
+import java.lang.Math;
 
 public class Dz1 {
     //1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
     public static int randomOut(int maxRandom) {
         Random outNum = new Random();
-        int outer = outNum.nextInt(maxRandom);
-        System.out.println(outer);
-        return outer;
+        return outNum.nextInt(maxRandom);
+    }
+
+    //2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+
+    public static int calcMSb(int num,int maxInput){
+        double log2num = Math.log(maxInput) / Math.log(2);
+        int rightShift = (int)log2num;
+        return num >> rightShift;
     }
 }
--- DZ1/Dz1/src/main/java/org/example/Main.java
+++ DZ1/Dz1/src/main/java/org/example/Main.java
@@ -2,7 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int i = Dz1.randomOut(2001);
-        System.out.println(i);
+        int maxInput = 2001;
+        int outRan = Dz1.randomOut(maxInput);
+        int msb = Dz1.calcMSb(outRan,maxInput);
+        System.out.println(outRan);
+        System.out.println(msb);
     }
 }
