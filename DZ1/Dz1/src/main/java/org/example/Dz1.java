f29634a9912d4b240e942c440f720a6cc113b93e
Anton Kruglyakov <akrugs@mail.ru>
2023-06-28 15:50:38 +0500

    Добавлены задания 3 и 4

--- DZ1/Dz1/src/main/java/org/example/Dz1.java
+++ DZ1/Dz1/src/main/java/org/example/Dz1.java
@@ -1,6 +1,9 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 import java.lang.Math;
+import java.lang.Short;
 
 public class Dz1 {
     //1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
@@ -11,9 +14,36 @@ public class Dz1 {
 
     //2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
 
-    public static int calcMSb(int num,int maxInput){
-        double log2num = Math.log(maxInput) / Math.log(2);
-        int rightShift = (int)log2num;
-        return num >> rightShift;
+    public static int calcMSb(int num){
+        double log2num = Math.log(num) / Math.log(2);
+        return (int)log2num;
+    }
+
+    //3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+
+    public static LinkedList<Short> sMaxValue(int i, int n){
+        int j = 0;
+        LinkedList<Short> massiv = new LinkedList<>();
+        for(;i<=Short.MAX_VALUE; i++) {
+            if(i%n==0) {
+                massiv.add((short)i);
+            }
+        }
+
+        return massiv;
+    }
+
+    //4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
+
+    public static LinkedList<Short> sMinValue(int i, int n){
+        int j = 0;
+        LinkedList<Short> massiv = new LinkedList<>();
+        for(;i>=Short.MIN_VALUE; i--) {
+            if(i%n==0) {
+                massiv.add((short)i);
+            }
+        }
+
+        return massiv;
     }
 }
--- DZ1/Dz1/src/main/java/org/example/Main.java
+++ DZ1/Dz1/src/main/java/org/example/Main.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         int maxInput = 2001;
         int outRan = Dz1.randomOut(maxInput);
-        int msb = Dz1.calcMSb(outRan,maxInput);
-        System.out.println(outRan);
-        System.out.println(msb);
+        int msb = Dz1.calcMSb(outRan);
+        LinkedList<Short> m1 = new LinkedList<>();
+        LinkedList<Short> m2 = new LinkedList<>();
+        m1 = Dz1.sMaxValue(outRan,msb);
+        m2 = Dz1.sMinValue(outRan,msb);
+        System.out.printf("Случайное целое число в диапазоне от 0 до %s: %s\n",maxInput-1,outRan);
+        System.out.printf("Старший значащий бит(0..n): %s\n",msb);
+        System.out.printf("Массив элементов, кратных %s, в диапазоне от %s до %s:\n %s\n",msb,outRan,Short.MAX_VALUE,m1);
+        System.out.printf("Массив элементов, кратных %s, в диапазоне от %s до %s:\n %s\n",msb,outRan,Short.MIN_VALUE,m2);
     }
 }
